6db12449c0000ff83d9cb3c0bbd40ed97f667d16
Yeison Esteban Pretel Puentes <43614257+yeprepue@users.noreply.github.com>
2023-05-11 12:38:23 -0500

    Creación de servidor y configuración de script
    Creación de servidor y configuración de script y asignación de puerto donde se va a escuchar la API.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 
-const app = express(); 
+
+const app = express();
+
+app.use(morgan('dev'))
+
+app.listen('3000')
+console.log('server un port 3000')
